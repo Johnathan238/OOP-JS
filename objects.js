0f49e76d06c4db86e00d7b8c0dc9629c6684192e
Johnathan238 <jonathanumana86@gmail.com>
2022-07-17 19:55:43 -0400

    Learning about ES6 with objects & keywork THIS

--- objects.js
+++ objects.js
@@ -1,6 +1,9 @@
 let userOne = {
   email: "FreshyyIDk@gmail.com",
-  name: "Freshyy"
+  name: "Freshyy",
+  login() {
+    alert(`${this.email}, ${this.name} has logined in succesfully!`)
+  }
 }
 
 console.log(userOne);
