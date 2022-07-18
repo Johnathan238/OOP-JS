a5647d6597f735a90cf0673de045f00c44f13930
Johnathan238 <jonathanumana86@gmail.com>
2022-07-18 02:01:14 -0400

    Understanf constructor & new keyword & class

--- objects.js
+++ objects.js
@@ -1,9 +1,31 @@
-let userOne = {
-  email: "FreshyyIDk@gmail.com",
-  name: "Freshyy",
-  login() {
-    alert(`${this.email}, ${this.name} has logined in succesfully!`)
+// let userOne = {
+//   email: "FreshyyIDk@gmail.com",
+//   name: "Freshyy",
+//   login() {
+//     console.log( this.name, "has logined in succesfully!")
+//   },
+//   logout() {
+//     console.log( this.name, "has logged out succesfully!")
+//   }
+// }
+
+// console.log(userOne.email);
+
+// Creating class !
+
+class User {
+  constructor(email, name) {
+    this.email = email
+    this.name = name
   }
 }
 
+let userOne = new User('FreshyyIDk@gmail.com', 'Freshyy')
+let userTwo = new User('IDkU@gmail.com', 'MarioJudahh')
 console.log(userOne);
+console.log(userTwo);
+
+// ~ the 'new' keyword
+// ~ creates a new empty object {}
+// ~ sets the value of 'this' to be the new empty object
+// ~ calls the constructor method
